39800d03c1baa8c2818bec7822a903ee2352339e
PylaevDmitry <pilaevdmitry@yandex.ru>
2022-07-05 08:48:39 +0400

    simplify WindowUserInterface, add Adapter

--- src/main/java/ru/pylaev/toDoProject/presentLayer/ui/WindowUserInterface.java
+++ src/main/java/ru/pylaev/toDoProject/presentLayer/ui/WindowUserInterface.java
@@ -4,11 +4,9 @@ import org.springframework.stereotype.Component;
 import ru.pylaev.toDoProject.businessLogicLayer.UserInputService;
 import ru.pylaev.toDoProject.presentLayer.ViewHandler;
 import ru.pylaev.toDoProject.presentLayer.view.View;
+import ru.pylaev.toDoProject.presentLayer.view.ViewToJScrollPaneWriter;
 
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 
 @Component
 public class WindowUserInterface extends UserInterface {
@@ -17,59 +15,40 @@ public class WindowUserInterface extends UserInterface {
             setTitle("TODO");
             setDefaultCloseOperation((WindowConstants.DISPOSE_ON_CLOSE));
             setBounds(300, 300, 900, 400);
-            add(field);
+            add(panel);
             setVisible(true);
         }
     }
 
-    private final JPanel field = new JPanel();
+    private final JTextField textField = new JTextField(72);
+    private final JPanel panel = new JPanel();
     private final MainWindow mainWindow = new MainWindow();
-
-    private boolean userInputEnds;
-
+    
     public WindowUserInterface(View view, UserInputService userInputService) {
         super(view, userInputService);
     }
 
     @Override
     public void run () {
-        final String[] userInput = new String[1];
-
-        JTextField textField = new JTextField(72);
         textField.setHorizontalAlignment(JTextField.CENTER);
         textField.addActionListener(e -> {
-            userInput[0] = textField.getText();
-            userInputEnds = true;
+            processTextField(textField);
             textField.setText("");
         });
 
-        while (true) {
-            List<String> list = new ArrayList<>();
-            if (!Objects.isNull(view.getArrTasks())) {
-                list.addAll(List.of(view.getArrTasks()));
-            }
-            list.add(view.getMessage());
-            field.removeAll();
-            DefaultListModel<String> dlm = new DefaultListModel<>();
-            for (int i = list.size()-1; i >=0 ; i--) {
-                dlm.add(0, list.get(i));
-            }
-            JList<String> jList = new JList<>(dlm);
-            jList.setFixedCellWidth(790);
-            field.add(new JScrollPane(jList));
-            userInputEnds = false;
-            field.add(textField);
-            field.repaint();
-            mainWindow.setVisible(true);
-            while (!userInputEnds) {
-                try {
-                    Thread.sleep(250);
-                } catch (InterruptedException ignored) {
-                }
-            }
-            ViewHandler.processUserInput(userInput[0], view, userInputService);
-        }
+        processTextField(textField);
+
+        try {while (true) {Thread.sleep(250);}}
+        catch (InterruptedException e) {e.printStackTrace();}
     }
 
+    private void processTextField(JTextField textField) {
+        ViewHandler.processUserInput(textField.getText(), view, userInputService);
 
+        panel.removeAll();
+        panel.add(ViewToJScrollPaneWriter.write(view));
+        panel.add(textField);
+        panel.repaint();
+        mainWindow.setVisible(true);
+    }
 }
---
+++ src/main/java/ru/pylaev/toDoProject/presentLayer/view/ViewToJScrollPaneWriter.java
@@ -0,0 +1,23 @@
+package ru.pylaev.toDoProject.presentLayer.view;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class ViewToJScrollPaneWriter {
+    public static JScrollPane write (View view) {
+        List<String> list = new ArrayList<>();
+        if (!Objects.isNull(view.getArrTasks())) {
+            list.addAll(List.of(view.getArrTasks()));
+        }
+        list.add(view.getMessage());
+        DefaultListModel<String> dlm = new DefaultListModel<>();
+        for (int i = list.size()-1; i >=0 ; i--) {
+            dlm.add(0, list.get(i));
+        }
+        JList<String> jList = new JList<>(dlm);
+        jList.setFixedCellWidth(790);
+        return new JScrollPane(jList);
+    }
+}
